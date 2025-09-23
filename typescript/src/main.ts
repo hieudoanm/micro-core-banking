148e04c9f2a1f67bec8140ddce826547cfca9398
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-09-23 15:10:50 +0700

    update typescript

---
+++ typescript/src/main.ts
@@ -0,0 +1,28 @@
+import { INestApplication } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+const PORT = process.env.PORT ?? 3000;
+
+const buildDocument = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('Micro Core Banking')
+    .setDescription('Micro Core Banking API')
+    .setVersion('0.1')
+    .addTag('accounts')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+};
+
+const bootstrap = async () => {
+  const app: INestApplication = await NestFactory.create(AppModule);
+  buildDocument(app);
+  await app.listen(PORT);
+};
+
+bootstrap().catch((error) =>
+  console.error('Error during app bootstrap:', error),
+);
