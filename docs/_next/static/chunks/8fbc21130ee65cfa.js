2dbded87614dd26e56d9deef6387d03a40825680
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-11-29 17:55:47 +0700

    add mobile

--- docs/_next/static/chunks/8fbc21130ee65cfa.js
+++ docs/_next/static/chunks/8fbc21130ee65cfa.js
@@ -2,6 +2,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/aa3e908ff12d7156.js",
   "static/chunks/8bedba256e28d2a8.js",
   "static/chunks/b69ed89cd3144a18.js",
-  "static/chunks/943e7c921de7ad3e.css",
-  "static/chunks/turbopack-a3dc6f5e42209ef1.js"
+  "static/chunks/82d2fb9b848b4c63.css",
+  "static/chunks/turbopack-cb49a726da626dc3.js"
 ])
