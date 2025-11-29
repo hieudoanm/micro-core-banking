041c3d1da018140a99573913e2c6f5149560d8f1
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-11-29 17:36:46 +0700

    add CI

---
+++ docs/_next/static/chunks/92354141cd37841d.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/aa3e908ff12d7156.js",
+  "static/chunks/8bedba256e28d2a8.js",
+  "static/chunks/b69ed89cd3144a18.js",
+  "static/chunks/943e7c921de7ad3e.css",
+  "static/chunks/turbopack-a3dc6f5e42209ef1.js"
+])
---
+++ docs/_next/static/zXuDFZEU5AV9gGw8aP-tr/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/18c115bf8fabdc60.js"
+  ],
+  "/_error": [
+    "static/chunks/7fc0b7b7d8d5d12f.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
